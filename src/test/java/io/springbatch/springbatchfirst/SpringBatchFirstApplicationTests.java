64341f48c031870c3b3cf15070b3c14553f6a89b
hong yuseok <jericoh@naver.com>
2022-09-18 17:38:06 +0900

    first commit

---
+++ src/test/java/io/springbatch/springbatchfirst/SpringBatchFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springbatch.springbatchfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
