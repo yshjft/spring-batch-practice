faef9b366d0cfcf1f9ca8bea9ebba507086182cb
hong yuseok <jericoh@naver.com>
2022-10-18 15:28:43 +0900

    JobParameters

--- src/main/java/io/springbatch/springbatchfirst/JobParameterTest.java
+++ src/main/java/io/springbatch/springbatchfirst/JobParameterTest.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
-// @Component
+@Component
 @RequiredArgsConstructor
 public class JobParameterTest implements ApplicationRunner {
     private final JobLauncher jobLauncher;
