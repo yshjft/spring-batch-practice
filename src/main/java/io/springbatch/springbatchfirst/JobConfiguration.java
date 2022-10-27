8fababe7958b09975a76bd2e3793689e198c4842
hong yuseok <jericoh@naver.com>
2022-10-27 12:01:05 +0900

    JobConfiguration

--- src/main/java/io/springbatch/springbatchfirst/JobConfiguration.java
+++ src/main/java/io/springbatch/springbatchfirst/JobConfiguration.java
@@ -2,7 +2,6 @@ package io.springbatch.springbatchfirst;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -15,13 +14,13 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class JobLauncherConfiguration {
+public class JobConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job job() {
-        return jobBuilderFactory.get("job")
+    public Job batchJob1() {
+        return jobBuilderFactory.get("batchJob1")
                 .start(step1())
                 .next(step2())
                 .build();
@@ -33,7 +32,7 @@ public class JobLauncherConfiguration {
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        Thread.sleep(3000);
+                        System.out.println("step1 has executed");
                         return RepeatStatus.FINISHED;
                     }
                 })
@@ -46,6 +45,7 @@ public class JobLauncherConfiguration {
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("step2 has executed");
                         return RepeatStatus.FINISHED;
                     }
                 })
